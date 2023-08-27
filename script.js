0f22174237cc01177fcce86a9d3e8117401d5aa1
JohannHolm <johannholm94@gmx.de>
2023-08-27 22:10:06 +0200

    sidebar, arrays, css

---
+++ script.js
@@ -0,0 +1,128 @@
+let pizzas: [
+  {
+    name: 'Pizza Salami',
+    price: '8',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Pizza Funghi',
+    price: '9',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Pizza Tonno',
+    price: '8',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Pizza Margherita',
+    price: '6',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Pizza Döner',
+    price: '8',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Pizza Ocean',
+    price: '14',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Pizza Paprika',
+    price: '8',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Pizza Diavolo',
+    price: '9',
+    description: 'Das leckerste vom feinsten und besten...',
+  }
+];
+let salads: [
+  {
+    name: 'Salat Cesare',
+    price: '12',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Salat Salmone',
+    price: '14',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Salat Ocean',
+    price: '12',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Salat Basic',
+    price: '9',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Salat Schafsbock',
+    price: '12',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Salat Oily',
+    price: '12',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Salat Farmer',
+    price: '10',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Salat Scampi',
+    price: '16',
+    description: 'Das leckerste vom feinsten und besten...',
+  }
+];
+let burger: [
+  {
+    name: 'Cheeseburger',
+    price: '6',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Double Cheeseburger',
+    price: '9',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Chickenburger',
+    price: '8',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Vegan-Mist Burger',
+    price: '16',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Triple-Cheeseburger',
+    price: '12',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'BBQ Burger',
+    price: '11',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Steak Burger',
+    price: '12',
+    description: 'Das leckerste vom feinsten und besten...',
+  },
+  {
+    name: 'Super Vegan Burger',
+    price: '16',
+    description: 'Das leckerste vom feinsten und besten...',
+  }
+];
+
+function generateMealBoxes() {}
